520db20a5dda3c7985c258d97118f54770ee82b8
hemilarose <hemilarose.28csa@licet.ac.in>
2026-06-26 19:29:59 +0530

    Move CORS middleware to the very top

--- PathFirst/backend/backend/settings.py
+++ PathFirst/backend/backend/settings.py
@@ -149,9 +149,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "https://vercel.app",
-    "http://localhost:3000",  
-    "http://127.0.0.1:3000",
+    "https://pathfirst-e1n9do879-hemilarose28csa-3262s-projects.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
